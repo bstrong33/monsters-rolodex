1984f46447ded4aa819efac0ebf4d1fc8ae07f83
bstrong33 <boston.strong33@gmail.com>
2022-05-24 17:47:49 -0400

    completed functionality

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
-import { Component } from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import CardList from './components/card-list/card-list';
+import SearchBox from './components/search-box/search-box';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
 
-    this.state = {
-      monsters:[
-        {
-          name: 'Linda'
-        },
-        {
-          name: 'Frank'
-        },
-        {
-          name: 'Jackie'
-        }
-      ]
-    }
+  const [monsters, setMonsters] = useState([])
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters)
+  const [searchField, setSearchField] = useState('')
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => setMonsters(users))
+  }, [])
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(searchField)
+    });
+
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField])
+
+  function onSearchChange(event) {
+    let searchFieldInput = event.target.value.toLowerCase()
+    setSearchField(searchFieldInput)
   }
 
-
-
-  render() {
-
-      const mappedMonsters = this.state.monsters.map((monster) => {
-        return (
-          <h1>{monster.name}</h1>
-        )
-      })
-
-    return (
-      <div className="App">
-        {mappedMonsters}
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1 className='app-title'>Monsters Rolodex</h1>
+      <SearchBox 
+        className='search-box'
+        placeholder='search monsters'
+        onChangeHandler={onSearchChange}
+      />
+      <CardList monsters={filteredMonsters}/>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/search-box/search-box.js
@@ -0,0 +1,14 @@
+import './search-box.css';
+
+const SearchBox = ({ className, placeholder, onChangeHandler }) => {
+    return (
+        <input 
+            type='search' 
+            className={className} 
+            placeholder={placeholder} 
+            onChange={onChangeHandler}
+        />
+    )
+}
+
+export default SearchBox;
